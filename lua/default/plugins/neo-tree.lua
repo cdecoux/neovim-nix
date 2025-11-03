cdbb8f652eb24e7615a4f8d18244c3a4cfacac81
Caleb DeCoux <cdecoux@protonmail.com>
2025-11-03 11:51:46 -0600

    cleanup kickstart, move neotree

--- lua/default/plugins/neo-tree.lua
+++ lua/default/plugins/neo-tree.lua
@@ -1,10 +1,7 @@
 -- Neo-tree is a Neovim plugin to browse the file system
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
-
 return {
   'nvim-neo-tree/neo-tree.nvim',
-  -- NOTE: nixCats: return true only if category is enabled, else false
-  enabled = require('nixCatsUtils').enableForCategory("kickstart-neo-tree"),
   version = '*',
   dependencies = {
     'nvim-lua/plenary.nvim',
